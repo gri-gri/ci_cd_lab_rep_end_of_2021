765c6cb081ab6a2ef9ef3752120cd74b0665c462
Grigoriy Simonov <gri_simonov@mail.ru>
2021-12-17 15:54:44 +0300

    Update setup.py for autobuilding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='',
+    version='1.0',
+    description='Copied from StackOverFlow',
+    author='Grigoriy Simonov',
+    author_email='gri_simonov@mail.ru',
+    packages=['code'], 
+    install_requires=['unittest', 'math']
+)
